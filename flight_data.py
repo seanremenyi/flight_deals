69b32105022dd39ea6fe1f33efd8de0e5946e0d7
Sean Remenyi <sean.remenyi@gmail.com>
2021-07-04 16:40:46 +1000

    twilio integrated

--- flight_data.py
+++ flight_data.py
@@ -1,3 +1,11 @@
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+
+    def __init__(self, price, origin_city, origin_airport,
+                 destination_city, destination_airport, leave_date, return_date):
+        self.price = price
+        self.origin_city = origin_city
+        self.origin_airport = origin_airport
+        self.destination_city = destination_city
+        self.destination_airport = destination_airport
+        self.leave_date = leave_date
+        self.return_date = return_date
--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-import requests
+from datetime import datetime, timedelta
 from data_manager import DataManager
-from flight_data import FlightData
 from flight_search import FlightSearch
 from notification_manager import NotificationManager
-from pprint import pprint
-
 
 data_manager = DataManager()
 data = data_manager.get_destinations_data()
-
 flight_search = FlightSearch()
+notification_manager = NotificationManager
+
+ORIGIN_CITY_IATA = "MEL"
+
 for row in data:
     row["iataCode"] = flight_search.get_codes(row['city'])
-
 data_manager.destination_data = data
 data_manager.update_sheet()
 
+tomorrow = datetime.now() + timedelta(days=1)
+six_months = datetime.now() + timedelta(days=(6 *30))
 
-
+for destination in data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_months
+    )
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to "
+                    f"{flight.destination_city}-{flight.destination_airport}, from {flight.out_date} "
+                    f"to {flight.return_date}."
+        )
 
 
